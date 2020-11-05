b3d97393ecb0eedc376b22acfdc7a378fe6e9407
Mark Martin <markmartinlopez13@gmail.com>
2020-11-05 16:42:04 +0100

    Update web

---
+++ Portfolio/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portfolio</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./style/index.css">
+</head>
+<body>
+  <div class="container-fluid bg-dark">
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-6">
+          <h1 class="display-4 font-italic text-white">Portfolio 2020-2021</h1>      
+      </div>
+      <div class="col-4">
+        <a href="index.php"><img id="imgheader" src="./media/bitmoji_mark.png"></a>
+      </div>
+    </div>  
+  </div>
+  <nav class="nav nav-pills flex-column flex-sm-row bg-dark">
+    <a class="flex-sm-fill text-sm-center nav-link border border-white text-white" href="m06.php">M06</a>
+    <a class="flex-sm-fill text-sm-center nav-link border border-white text-white" href="m09.php">M09</a>
+    <a class="flex-sm-fill text-sm-center nav-link border border-white text-white" href="m12.php">M12</a>
+  </nav>
+  <div class="container-fluid bg-dark">
+    <div class="row content"></div>
+    <div class="row content">
+      <div class="col-2"></div>
+      <div class="col-6">
+        <p id="texto" class="texto text-white">Soy Mark Martin, un alumno de 21 años del Centre d'Estudis Politecnics.</p>
+        <p class="texto text-white">Estoy cursando el grado superior de Desarrollo de Aplicaciones Web 
+          y en esta pagina iré actualizando los trabajos y proyectos que vaya desarrollando durante el curso.</p>
+      </div>
+      <div class="col-4">
+        <img id="imgcontent" src="./media/7kmhZsBf.jpg">
+      </div>
+    </div>
+  </div> 
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</html>
---
+++ Portfolio/m06.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Portfolio</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./style/m06.css">
+</head>
+<body>
+  <div class="container-fluid bg-dark">
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col-7">
+          <h1 class="display-4 font-italic text-white">Desarrollo web entorno cliente</h1>      
+      </div>
+      <div class="col-4">
+        <a href="index.php"><img id="imgheader" src="./media/bitmoji_mark.png"></a>
+      </div>
+    </div>  
+  </div>
+  <nav class="nav nav-pills flex-column flex-sm-row bg-dark">
+    <a class="flex-sm-fill text-sm-center nav-link border border-white text-white active" href="m06.php">M06</a>
+    <a class="flex-sm-fill text-sm-center nav-link border border-white text-white" href="m09.php">M09</a>
+    <a class="flex-sm-fill text-sm-center nav-link border border-white text-white" href="m12.php">M12</a>
+  </nav>
+  <div class="container-fluid bg-dark">
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-6">
+            <h2 class="display-4 font-italic text-white">RE-COMERÇEM</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <pre id="texto" class="font-italic text-white">
+                Este proyecto tiene como fin divulgar y potenciar los mercados locales 
+                a través de una aplicación  WEB, en concreto, nos enfocaremos en el 
+                Mercat de la Concepció, que por causa de la Covid se han visto afectados.
+
+                -MERCADO DE LAS FLORES 0:14: Mercado muy famoso en Barcelona por su particularidad 
+                de venta de flores las 24hr del día los 365 días del año.
+                -MULTIMEDIA 1:42: Enganchar a futurxs clientxs a través de la promoción,
+                contaremos un poco  de la historia del juego, eventos especiales,
+                las paradas que se encuentran en el Mercado y un Mapa para poder llegar.
+                -JUEGOS 3:11: Cuatro juegos con una historia lineal para enganchar al usuarix 
+                ganando puntos para promociones y ofertas.
+                -CUENTAS 5:02: 
+                    · Usurix→ Tendrán que registrarse para poder jugar y acumular puntos/promociones 
+                    para luego canjearlos, 
+                    podrán hacer su wish list y llegar a sus objetivos.
+                    · Administración → Panel para administrar usuarixs y admins, dar de alta 
+                    y editar nuevas promociones,
+                    editar los puntos ganados en cada juego, ver el historial de juego, 
+                    entre otras opciones.
+                -CONTACTO 7:13: Contacto del grupo,  un formulario reservado para preguntas/sugerencias 
+                de lxs clientxs y un mapa del centro(CEP).
+                -IDIOMA 7:27: 3 idiomas para intentar abarcar a más público, incluyendo turistas.
+                -SOBRE NOSOTRXS 7:43: Una pequeña descripción del grupo, CEP y de cada unx 
+                de sus integrantxs.
+                -FAQ 7:59: Preguntas frecuente
+            </pre>
+        </div>
+        <div class="col-8">
+            <a href="https://coggle.it/diagram/X418quxmjwJza8oQ/t/re-comer%C3%A7em/7c1e61979416b48d8909725d2b0abe4e2904523ad022aa87117905d926acdf99"><img id="imgcoogle" src="./media/coogle.png" alt="Haz click para verlo en coogle."></a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/10aLCRLOMFw" allowfullscreen></iframe>
+          </div>
+        </div>
+    </div>
+  </div> 
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</html>
